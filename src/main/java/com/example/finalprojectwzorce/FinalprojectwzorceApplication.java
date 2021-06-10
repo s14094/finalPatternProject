a64d07d2e2acf7eef7f2eeacd6be1eb7bf29e277
Paweł Łakomiec <pawellakomiec95@gmail.com>
2021-06-10 20:33:55 +0200

    init

---
+++ src/main/java/com/example/finalprojectwzorce/FinalprojectwzorceApplication.java
@@ -0,0 +1,27 @@
+package com.example.finalprojectwzorce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+        budowniczy, fabryka,
+                fasada,
+                stan, strategia
+
+        symulator walki z bossem w jakiejs grze
+        2 typy klas
+
+        dylemat wieznia jako walka
+ */
+
+
+@SpringBootApplication
+public class FinalprojectwzorceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalprojectwzorceApplication.class, args);
+
+        
+    }
+
+}
---
+++ src/test/java/com/example/finalprojectwzorce/FinalprojectwzorceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectwzorce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectwzorceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
